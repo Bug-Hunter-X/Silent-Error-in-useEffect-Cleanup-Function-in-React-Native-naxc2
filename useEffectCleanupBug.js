f77d7d5ce23e5dd8d78e5e4f1822fda1a4955128
BugHunter-X <askzenai@gmail.com>
2025-01-03 09:52:06 +0100

    Add useEffectCleanupBug.js file

---
+++ useEffectCleanupBug.js
@@ -0,0 +1,1 @@
+This error occurs when using the `useEffect` hook in React Native with a cleanup function that throws an error. This can happen if the cleanup function tries to access or modify a component state or prop that no longer exists because the component has unmounted.  This often manifests as a silent failure with no clear error message in the console.
